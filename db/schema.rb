f9248da93ff9409d2a52896fa78c33045f5b3fb6
vagrant <vagrant@localhost.localdomain>
2018-04-12 05:00:37 +0000

    プロフィール画像（初期設定）

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410011832) do
+ActiveRecord::Schema.define(version: 20180412015111) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
